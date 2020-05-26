85a8cdadfa979f3dcc552576d98e7ca4e651a305
Lukortech <lukortech@wp.pl>
2020-05-26 17:50:31 +0200

    added new view engine to show React files properly

--- index.js
+++ index.js
@@ -4,63 +4,53 @@ const http = require("http");
 const WebSocket = require("ws");
 
 // Setup
+
 const port = 3000;
-const colorRegexp = new RegExp("^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$");
+const options = {
+  root: path.join(__dirname, "public"),
+  dotfiles: "deny",
+  headers: {
+    "x-timestamp": Date.now(),
+    "x-sent": true,
+  },
+};
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+let bgColor;
 app.use(express.static("public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+// Web socket part
 
 wss.on("connection", (ws) => {
   ws.on("message", (msg) => {
-    let strArr = msg.split(" ");
-    let foundColor = strArr.filter((e) => e.match(colorRegexp));
-
-    if (foundColor) {
-      ws.send(`The color we think you've sent -> ${foundColor[0]}`);
-    } else {
-      ws.send(`We think you've sent no color code.`);
-    }
-  });
-  ws.send("We're waiting for your color code!");
-});
-
-app.get("/colors/:color", (req, res, next) => {
-  const options = {
-    root: path.join(__dirname, "public"),
-    dotfiles: "deny",
-    headers: {
-      "x-timestamp": Date.now(),
-      "x-sent": true,
-    },
-  };
-
-  const colorName = req.params.color;
-  res.sendFile("colors.html", options, (err) => {
-    if (err) next(err);
-    console.log("Sent:", colorName);
+    ws.send(`There was a color submission: ${msg}`);
+    bgColor = msg; // I don't need to check it's value since the input field is sanitizing the data sent to server.
   });
 });
+
+// Routing
 
 app.get("/", (req, res, next) => {
-  const options = {
-    root: path.join(__dirname, "public"),
-    dotfiles: "deny",
-    headers: {
-      "x-timestamp": Date.now(),
-      "x-sent": true,
-    },
-  };
-
   res.sendFile("index.html", options, (err) => {
     if (err) next(err);
   });
 });
 
-// app.listen(port, () =>
-//   console.log(`Example app listening at http://localhost:${port}`)
-// );
+app.get("/myBG", (req, res, next) => {
+  // res.send("colors.html", { ...options, data: bgColor });
+  //   res.sendFile("colors.html", options, (err) => {
+  //     if (err) next(err);
+  //   });
+});
+
+app.get("/home", (req, res) => {
+  res.render("Colors", { bgColor });
+}); // A way to show you that you could easily transfer to React if needed ;)
 
 server.listen(process.env.PORT || port, () => {
   console.log(`Server started on port ${server.address().port} :)`);
